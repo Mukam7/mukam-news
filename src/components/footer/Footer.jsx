a7914356d088a776893cd8192b5bcb4607ef9448
Mukam7 <mukamhojaniyazow89@gmail.com>
2023-07-15 13:51:53 +0500

    last commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import "./Footer.css"
+
+
+const Footer = () => {
+  return (
+    <div className="container">
+      <div className="footer my-3">
+        <div className="footer-carts">
+          <div className="footer-card">
+            <li>Lorem.</li>
+            <li>Lorem.</li>
+            <li>Lorem.</li>
+            <li>Lorem.</li>
+          </div>
+          <div className="footer-card">
+            <li>Lorem.</li>
+            <li>Lorem.</li>
+            <li>Lorem.</li>
+            <li>Lorem.</li>
+          </div>
+          <div className="footer-card">
+            <li>Lorem.</li>
+            <li>Lorem.</li>
+            <li>Lorem.</li>
+            <li>Lorem.</li>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
